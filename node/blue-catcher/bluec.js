d02b67f79ecb72e11caa1c964f2bf8c87b79ae71
Bruno Pereira <bruno9pereira@gmail.com>
2016-10-26 13:11:57 -0200

    Require Args module for separation of concerns

--- node/blue-catcher/bluec.js
+++ node/blue-catcher/bluec.js
@@ -4,27 +4,7 @@ const request = require('request');
 const exec = require('child_process').exec;
 const sys = require('sys');
 
-const argv = require('yargs')
-    .option('owner', {
-        alias: 'o',
-        demand: true,
-        describe: 'Owner of',
-        type: 'string'
-    })
-    .option('username', {
-        alias: 'u',
-        demand: true,
-        describe: 'username for authentication',
-        type: 'string'
-    })
-    .option('password', {
-        alias: 'p',
-        demand: true,
-        describe: 'password for authentication',
-        type: 'string'
-    })
-    .help('help')
-    .argv;
+const argv = require('./args');
 
 const owner =  argv.owner;
 const options = {
