1d36b3dba6aa211b398763f39ec5a168a4be5c05
Bruno Pereira <bruno9pereira@gmail.com>
2016-10-26 13:05:00 -0200

    Update bluec.js

--- node/blue-catcher/bluec.js
+++ node/blue-catcher/bluec.js
@@ -3,12 +3,35 @@
 const request = require('request');
 const exec = require('child_process').exec;
 const sys = require('sys');
-let owner =  '';
+
+const argv = require('yargs')
+    .option('owner', {
+        alias: 'o',
+        demand: true,
+        describe: 'Owner of',
+        type: 'string'
+    })
+    .option('username', {
+        alias: 'u',
+        demand: true,
+        describe: 'username for authentication',
+        type: 'string'
+    })
+    .option('password', {
+        alias: 'p',
+        demand: true,
+        describe: 'password for authentication',
+        type: 'string'
+    })
+    .help('help')
+    .argv;
+
+const owner =  argv.owner;
 const options = {
     url: `https://bitbucket.org/!api/1.0/users/${owner}`,
     auth: {
-      user: '',
-      pass: ''
+      user: argv.username,
+      pass: argv.password
     }
 };
 
